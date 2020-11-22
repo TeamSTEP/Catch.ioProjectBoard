e34e46d36a1ba99420e100ba5e163d553de3dc5b
Hoon Kim <hoonsubin@icloud.com>
2020-11-21 20:18:41 -0800

    added homepage boilerplate

---
+++ homepage/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <Navbar />
+    <router-view />
+    <Footer />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Navbar from '@/components/Navbar.vue'; // @ is an alias to /src
+import Footer from '@/components/Footer.vue';
+
+@Options({
+    components: {
+        Navbar,
+        Footer,
+    },
+})
+export default class App extends Vue {}
+</script>
