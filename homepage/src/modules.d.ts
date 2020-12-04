0909102d6a9cda7cff43a38500fd37b9a6342d10
Hoon Kim <hoonsubin@icloud.com>
2020-12-04 02:32:31 -0800

    fixed module import

--- homepage/src/modules.d.ts
+++ homepage/src/modules.d.ts
@@ -1,6 +1,6 @@
-import { DefineComponent } from 'vue';
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'vue-scroll-snap' {
+    import { DefineComponent } from 'vue';
     const VueScrollSnap: DefineComponent<{}, {}, any>;
 
     export default VueScrollSnap;
