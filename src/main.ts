55e040422d81182db26848c8023ab845c18cc8e1
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-05-07 17:40:36 +0900

    resolved conflicts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
 import { createApp } from 'vue';
 import VueRellax from 'vue-rellax';
 import App from './App.vue';
-import './registerServiceWorker';
 import './assets/styles/base.css';
-import router from './router';
-import store from './store';
 
-<<<<<<< Updated upstream
-import Agile from 'vue-agile';
-
-createApp(App)
-    .use(store)
-    .use(router)
-    .use(Agile)
-    .mount('#app');
-=======
 createApp(App).use(VueRellax).mount('#app');
->>>>>>> Stashed changes
