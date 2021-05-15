35aa103599b1814665b79de82eeee06f95c64127
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-05-15 15:44:50 +0900

    changed parallax component position

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
     <navbar />
-    <mast-head />
+    <parallax-image />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Navbar from '@/components/Navbar.vue';
-import MastHead from '@/components/MastHead.vue';
+import ParallaxImage from '@/components/ParallaxImage.vue';
 
 export default defineComponent({
     name: 'App',
     components: {
         Navbar,
-        MastHead,
+        ParallaxImage,
     },
 });
 </script>
