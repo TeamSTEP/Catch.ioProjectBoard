40905f16650631f2e10d4844ccabbe45dffc85f6
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-05-30 01:58:30 +0900

    Update Config.ts

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   site_name: 'Witch One',
   title: 'Witch One | Team STEP',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  description: 'Stealth-focused fantasy battle-royal game',
   locale: 'en',
 };
